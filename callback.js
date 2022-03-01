4c4ba2882cf6336140e4b127077141b377603af8
401 <401@academicos.cesde.edu.co>
2022-03-01 11:09:10 -0500

    CB010 Se ha realizado un segundo ejercicio con callback

--- callback.js
+++ callback.js
@@ -1,3 +1,5 @@
+//Ejercicio 1
+
 /*function funcionPrincipal(callback){
 
     setTimeout(function(){
@@ -11,7 +13,7 @@ funcionPrincipal(function(){
     console.log("Soy la segunda funcion ")
 })*/
 
-function sumar(numero1,numero2,callback){
+/*function sumar(numero1,numero2,callback){
 // declaracion de la funcion principal
     setTimeout(function(){    
         let suma= numero1+numero2 
@@ -38,6 +40,47 @@ sumar(34,45,function(suma,numero1,numero2){
 
     //3.muestro la resta
     console.log(`La resta de ${numero1} y ${numero2} es igual a ${resta}`)
-})
+})*/
+
+
+//Ejercicio 2
+
+/* 1. reciba nombre,edad,password
+    2. construir un objeto con dicha información
+    3. funcion alterna verifique si el usuario es mayor o menor de edad
+    e imprima despues de 3 segundos, si si lo deja acceder o no a la pagina*/
+
+
+    function RecibirUsuario(nombre,edad,password,callback){
+
+
+        setTimeout(function(){
+
+            let usuario={
+
+                nombre:nombre,
+                edad:edad,
+                password:password
+            }
+
+           callback(usuario) 
+
+        },3000)  
+
+    }
+
+    RecibirUsuario(`Valentina`,22,`valentinaAmor`,function(usuario){
+
+        if(usuario.edad>= 18){
+            console.log( "Bienvenid@: " + usuario.nombre +  ",eres mayor de edad, puedes acceder a la pagina webcam ")
+           
+        }else{
+            console.log(usuario.nombre+ "no eres mayor de edad,no puedes acceder a la pagina webcam ")
+          
+        }
+    })
+
+
+    
 
 
